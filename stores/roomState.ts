d8a9998687320ce4bcb65cf2bc4eea71be9cb8fc
Leindfraust <rozer223@gmail.com>
2023-07-23 20:55:11 +0800

    chore: unused store

--- stores/roomState.ts
+++
@@ -1,8 +0,0 @@
-import type { RoomState } from 'types'
-
-import { defineStore } from 'pinia'
-export const useRoomStoreState = defineStore('roomState', {
-    state: () => ({
-        room: [] as RoomState[]
-    })
-})
