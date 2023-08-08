74816735e7b3da7408f481522f25bf8ca3f7fb12
Leindfraust <rozer223@gmail.com>
2023-08-08 16:55:56 +0800

    refactor: proper join and leave also reconnection

--- pages/room/[uid].vue
+++ pages/room/[uid].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useUserStoreState } from '@/stores/userState'
+import { useRoomStoreState } from '@/stores/roomState';
 import { ChatMessage } from 'types';
 definePageMeta({
     middleware: [
@@ -13,16 +14,14 @@ definePageMeta({
     layout: 'navigation'
 })
 
+const route = useRoute()
+const router = useRouter()
+const roomStateStore = useRoomStoreState()
 const userStateStore = useUserStoreState()
 const socket = useSocket()
 
 const chatMessages = ref<ChatMessage[]>([] as ChatMessage[])
 const inputMessage = ref('')
-
-onBeforeMount(() => {
-    socket.emit('join room', userStateStore.user.roomUid, userStateStore.user)
-
-})
 
 function autoScrollChatContainer() {
     setTimeout(() => {
@@ -32,6 +31,15 @@ function autoScrollChatContainer() {
 }
 
 onMounted(() => {
+    //reconnect user
+    if (Object.keys(roomStateStore.room).length === 0) {
+        socket.emit('join room', route.params.uid, userStateStore.user)
+    }
+
+    socket.on('force disconnect', () => {
+        router.push('/')
+    })
+
     socket.on('global message', (msg, userId) => {
         if (userId === userStateStore.user.uid) {
             chatMessages.value = []
@@ -51,20 +59,11 @@ onMounted(() => {
         })
         autoScrollChatContainer()
     })
-
-    window.addEventListener("beforeunload", function (event) {
-        socket.emit('leave room', userStateStore.user.roomUid, userStateStore.user)
-        socket.disconnect()
-    });
-})
-onBeforeUnmount(() => {
-    socket.emit('leave room', userStateStore.user.roomUid, userStateStore.user)
-    socket.disconnect()
 })
 
 function sendMessage() {
     if (inputMessage.value !== '') {
-        socket.emit('message', userStateStore.user.roomUid, userStateStore.user, inputMessage.value)
+        socket.emit('message', route.params.uid, userStateStore.user, inputMessage.value)
         inputMessage.value = ''
     }
 }
@@ -91,25 +90,18 @@ function sendMessage() {
                     <div class="chat-bubble text-lg">{{ message.message }}</div>
                 </div>
                 <div class="chat chat-end" v-else-if="message.senderId !== 'system'">
-                    <div class="chat-image avatar">
-                        <div class="w-14 rounded-full">
-                            <img :src="`https://api.dicebear.com/6.x/adventurer-neutral/svg?seed=${message.senderId}`" />
-                        </div>
-                    </div>
-                    <div class="chat-header">
-                        {{ message.username }}(You)
-                    </div>
                     <div class="chat-bubble text-lg">{{ message.message }}</div>
                 </div>
             </span>
         </div>
         <div class="flex items-center justify-center">
-            <div class="fixed bottom-0 p-12 w-9/12">
-                <div class="join flex items-center justify-center">
-                    <input class="input input-bordered join-item lg:w-11/12 md:w-full" placeholder="Type here..."
+            <div class="fixed bottom-0 p-12 lg:w-9/12">
+                <div class="flex items-center justify-center space-x-4">
+                    <!-- <textarea class="textarea textarea-bordered textarea-lg join-item w-11/12" placeholder="Type here..."
+                        v-model="inputMessage" @keyup.enter="sendMessage"></textarea> -->
+                    <input type="text" placeholder="Type here..." class="input input-lg join-item lg:w-screen xs:w-screen"
                         v-model="inputMessage" @keyup.enter="sendMessage" />
-                    <button class="btn join-item rounded-r-full" @click="sendMessage"
-                        :disabled="inputMessage == ''">Send</button>
+                    <font-awesome-icon icon="paper-plane" size="2xl" class=" cursor-pointer" @click="sendMessage" />
                 </div>
             </div>
         </div>
@@ -117,19 +109,6 @@ function sendMessage() {
 </template>
 <style scoped>
 ::-webkit-scrollbar {
-    width: 12px;
-}
-
-::-webkit-scrollbar-track {
-    background-color: #000;
-}
-
-::-webkit-scrollbar-thumb {
-    border-radius: 6px;
-    background-color: #777;
-}
-
-::-webkit-scrollbar-thumb:hover {
-    background-color: #999;
+    display: none;
 }
 </style>
