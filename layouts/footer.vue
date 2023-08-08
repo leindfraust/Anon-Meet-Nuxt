4fd228cbc09917860a20902f80f9cf18946887e5
Leindfraust <rozer223@gmail.com>
2023-08-08 17:16:20 +0800

    Revert "Merge branch 'main' into feat/dynamic-room-invite"
    This reverts commit 569186590afba214eff81b2a3f8dbb2033cd415d, reversing
    changes made to ad978412b2943d643a44cf66ec45572f651a98e1.

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -3,7 +3,7 @@
     <footer class="footer items-center p-8 bg-base-300 text-neutral-content">
         <div class="items-center grid-flow-col">
             <p>Open-source and free, created by <a class="cursor-pointer font-bold"
-                    href="https://github.com/leindfraust" target="_blank">Ronan</a></p>
+                    href="https://github.com/leindfraust" target="_blank">Ronan(leindfraust)</a></p>
         </div>
         <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
             <a class=" cursor-pointer" href="https://github.com/leindfraust/Anon-Meet-Nuxt" target="_blank"><font-awesome-icon
