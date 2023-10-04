ab9fa04078619594c27d496558dd345cecebf230
Romel Jr (Leindfraust) Zerna <rozer223@gmail.com>
2023-10-04 11:04:25 +0800

    chore: format

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -2,12 +2,13 @@
     <slot />
     <footer class="footer items-center p-8 bg-base-300 text-neutral-content">
         <div class="items-center grid-flow-col">
-            <p>Open-source and free, created by <a class="cursor-pointer font-bold"
-                    href="https://github.com/leindfraust" target="_blank">Ronan</a></p>
+            <p>Open-source and free, created by <a class="cursor-pointer font-bold" href="https://github.com/leindfraust"
+                    target="_blank">Ronan</a></p>
         </div>
         <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-            <a class=" cursor-pointer" href="https://github.com/leindfraust/Anon-Meet-Nuxt" target="_blank"><font-awesome-icon
-                    icon="fa-brands fa-github" size="2xl" /></a>
+            <a class=" cursor-pointer" href="https://github.com/leindfraust/Anon-Meet-Nuxt" target="_blank">
+                <font-awesome-icon icon="fa-brands fa-github" size="2xl" />
+            </a>
         </div>
     </footer>
 </template>
