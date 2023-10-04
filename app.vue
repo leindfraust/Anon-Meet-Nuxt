86d1a9f45cb96aefdb3b2a4c974a650e73e65611
Romel Jr (Leindfraust) Zerna <rozer223@gmail.com>
2023-10-04 11:02:23 +0800

    fix: uid are null upon disconnect

--- app.vue
+++ app.vue
@@ -14,8 +14,9 @@ const roomStateStore = useRoomStoreState()
 const route = useRoute()
 
 onBeforeMount(() => {
-  if (Object.keys(userStateStore.user).length === 0) {
+  if (Object.keys(userStateStore.user).length === 0 || !userStateStore.user.uid) {
     socket.on('session', ({ uid }) => {
+      console.log(uid)
       socket.auth = { uid }
       userStateStore.$patch({
         user: {
--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,8 @@ async function joinDefaultRoom() {
                 roomUid: '1a',
             }
         })
-        let user = username.value
+        const user = username.value
+        console.log(user)
         socket.auth = { user }
         socket.emit('join room', '1a', userStateStore.user)
         await router.push('/room/1a')
@@ -27,7 +28,7 @@ async function joinDefaultRoom() {
 }
 
 onMounted(() => {
-    if (Object.keys(userStateStore.user).length !== 0) {
+    if (!userStateStore.user.uid) {
         socket.emit('leave room', userStateStore.user.roomUid, userStateStore.user)
         userStateStore.$reset()
     }
